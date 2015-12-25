08c16bdef0318c603e5d6d34756f53bbb2e113d4
SiddhaartS <siddhaarthnellayil@gmail.com>
2015-12-25 15:30:03 +0530

    Initial commit with connectivity solved

--- App1/bin/Debug/AppX/js/dataExample.js
+++ App1/bin/Debug/AppX/js/dataExample.js
@@ -1,16 +1,8 @@
 ﻿(function () {
     "use strict";
 
-    var dataArray = [
-    { title: "Basic banana", text: "Low-fat frozen yogurt", picture: "images/60banana.png" },
-    { title: "Banana blast", text: "Ice cream", picture: "images/60banana.png" },
-    { title: "Brilliant banana", text: "Frozen custard", picture: "images/60banana.png" },
-    { title: "Orange surprise", text: "Sherbet", picture: "images/60orange.png" },
-    { title: "Original orange", text: "Sherbet", picture: "images/60orange.png" },
-    { title: "Vanilla", text: "Ice cream", picture: "images/60vanilla.png" },
-    { title: "Very vanilla", text: "Frozen custard", picture: "images/60vanilla.png" },
-    { title: "Marvelous mint", text: "Gelato", picture: "images/60mint.png" },
-    { title: "Succulent strawberry", text: "Sorbet", picture: "images/60strawberry.png" }
+   var dataArray = [
+
     ];
 
     var dataList = new WinJS.Binding.List(dataArray);
@@ -21,6 +13,6 @@
         {
             itemList: dataList
         };
-    WinJS.Namespace.define("DataExample", publicMembers);
-
+    WinJS.Namespace.define("DataExample", publicMembers)
+    //console.log(ParseObjects);
 })();
--- App1/bin/Debug/AppX/js/default.js
+++ App1/bin/Debug/AppX/js/default.js
@@ -6,15 +6,48 @@
 	var app = WinJS.Application;
 	var activation = Windows.ApplicationModel.Activation;
 
+	var responseString;
+
 	app.onactivated = function (args) {
 		if (args.detail.kind === activation.ActivationKind.launch) {
 			if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
-				// TODO: This application has been newly launched. Initialize your application here.
+			    // TODO: This application has been newly launched. Initialize your application here.
+			    var username = "qqnehcGoop2eo4bIQfhE8eXsxBA6aejlrakt0J3u";
+			    var password = "javascript-key=sZBrnv7KevAzPEA8JeLRLYV0uAGMir4xsuN4Zn9S";
+
+			    WinJS.xhr({
+			        headers: { "Content-type": "application/json", "Authorization": "Basic " + btoa(username + ":" + password) },
+			        url: "https://api.parse.com/1/classes/mun", type: "get", responseType: "JSON"
+			    }).then(
+                    function (success) {
+                        responseString = JSON.parse(success.response);
+                        console.log(responseString.results);
+                        var dataList = new WinJS.Binding.List(responseString.results);
+
+                        // Create a namespace to make the data publicly
+                        // accessible. 
+                        var publicMembers =
+                            {
+                                itemList: dataList
+                            };
+                        document.getElementById("basicListView").winControl.itemTemplate = document.getElementById("parseObjectListIconTextTemplate");
+                        document.getElementById("basicListView").winControl.itemDataSource = dataList.dataSource;
+
+
+                    },
+                   function (error) {
+                       console.log(error);
+                   }
+                    );
 			} else {
 				// TODO: This application was suspended and then terminated.
 				// To create a smooth user experience, restore application state here so that it looks like the app never stopped running.
 			}
 			args.setPromise(WinJS.UI.processAll());
+
+		    // Retrieve the button and register our event handler. 
+			var helloButton = document.getElementById("helloButton");
+			helloButton.addEventListener("click", buttonClickHandler, false);
 		}
 	};
 
@@ -24,5 +57,15 @@
 		// If you need to complete an asynchronous operation before your application is suspended, call args.setPromise().
 	};
 
+	function buttonClickHandler(eventInfo) {
+	  /*  var req = new XMLHttpRequest();
+	    req.onreadystatechange = function () {
+	        if (req.readyState == 4) {
+	            console.log(req.status);
+	        }
+	    };
+	    req.open("GET", "https://qqnehcGoop2eo4bIQfhE8eXsxBA6aejlrakt0J3u:javascript-key=sZBrnv7KevAzPEA8JeLRLYV0uAGMir4xsuN4Zn9S@api.parse.com/1/classes/mun", true);
+	    req.send();*/
+	}
 	app.start();
 })();
